6b4538a57e6cc916e677f318aa9cd08fc6cab3a9
axrolxojayev <saxrolxojayev@gmail.com>
2024-12-02 11:41:16 +0000

    ex3.1.s

---
+++ ex8a/ex3/ex3.1.s
@@ -0,0 +1,9 @@
+.global 
+_start_start:
+    MOV     R0, #0
+    LDR     R1, =0x40000000
+loop:    STR     R0, [R1]
+    ADD     R0, R0, #1    
+    ADD     R1, R1, #4
+    CMP     R0, #10    
+    BLT     loop
